f392a7e31621f5ab7f5eb8a597d732cf95f11a8b
KeepsItCrypt0 <PLSstrategy@gmail.com>
2025-05-07 01:17:53 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,18 +1,4 @@
-export const RPC_URLS = [
-  {
-    name: 'Infura',
-    url: 'https://mainnet.infura.io/v3/0c7b379c34424040826f02574f89b57d'
-  },
-  {
-    name: 'Alchemy',
-    url: 'https://eth-mainnet.g.alchemy.com/v2/60nF9qKWaj8FPzlhEuGUmam6bn2tIgBN'
-  },
-  {
-    name: 'Ankr',
-    url: 'https://rpc.ankr.com/eth/8d7581cb1a742b4ebd60ddb0ff4049a193726fef2999a3acb4dc53293cf089b1'
-  }
-];
-export const DEFAULT_RPC = 'https://eth-mainnet.g.alchemy.com/v2/60nF9qKWaj8FPzlhEuGUmam6bn2tIgBN'; // Fallback to Alchemy
+export const RPC_URL = 'https://eth-mainnet.g.alchemy.com/v2/60nF9qKWaj8FPzlhEuGUmam6bn2tIgBN'; // Alchemy
 export const CONTRACT_ADDRESS = '0x6c1dA678A1B615f673208e74AB3510c22117090e';
 export const CONTRACT_ABI = []; // In src/abi/PulseStrategy.json
 export const STAKED_PLS_ADDRESS = '0x0181e249c507d3b454dE2444444f0Bf5dBE72d09';
