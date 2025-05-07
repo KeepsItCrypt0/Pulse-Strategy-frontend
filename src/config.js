2b0e19ed034aaa5a6607f9d15795282a343ee264
KeepsItCrypt0 <PLSstrategy@gmail.com>
2025-05-07 01:46:31 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 export const RPC_URL = 'https://eth-mainnet.g.alchemy.com/v2/60nF9qKWaj8FPzlhEuGUmam6bn2tIgBN'; // Alchemy
 export const CONTRACT_ADDRESS = '0x6c1dA678A1B615f673208e74AB3510c22117090e';
-export const CONTRACT_ABI = []; // In src/abi/PulseStrategy.json
 export const STAKED_PLS_ADDRESS = '0x0181e249c507d3b454dE2444444f0Bf5dBE72d09';
