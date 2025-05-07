96f93c402ba45a0a2686da3f46b8b74f00c6d3a1
KeepsItCrypt0 <PLSstrategy@gmail.com>
2025-05-06 22:44:23 -0500

    Update AdminPanel.js

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,17 +1,19 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Card } from '@chakra-ui/react';
 import DepositStakedPLS from './DepositStakedPLS';
-import MintShares from './MintShares';
+import MintPLSTR from './MintPLSTR';
 import RecoverTokens from './RecoverTokens';
 import TransferOwnership from './TransferOwnership';
 
 const AdminPanel = ({ contract, account, signer }) => (
-  <Box mb={6} p={4} borderWidth="1px" borderRadius="md">
-    <Heading size="md" mb={4}>Admin Panel</Heading>
-    <DepositStakedPLS contract={contract} account={account} signer={signer} />
-    <MintShares contract={contract} account={account} />
-    <RecoverTokens contract={contract} account={account} />
-    <TransferOwnership contract={contract} account={account} />
-  </Box>
+  <Card mb={6}>
+    <Box p={4}>
+      <Heading size="md" mb={4}>Admin Panel</Heading>
+      <DepositStakedPLS contract={contract} account={account} signer={signer} />
+      <MintPLSTR contract={contract} account={account} />
+      <RecoverTokens contract={contract} account={account} />
+      <TransferOwnership contract={contract} account={account} />
+    </Box>
+  </Card>
 );
 
 export default AdminPanel;
