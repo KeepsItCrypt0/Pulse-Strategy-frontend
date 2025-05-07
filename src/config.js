918a2fc4c23637b5485ce79ec608bd2e0d1ea963
KeepsItCrypt0 <PLSstrategy@gmail.com>
2025-05-07 00:58:39 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
-import PulseStrategyABI from './abi/PulseStrategy.json';
-
+export const RPC_URLS = [
+  'https://mainnet.infura.io/v3/0c7b379c34424040826f02574f89b57d', // Infura
+  'https://eth-mainnet.g.alchemy.com/v2/60nF9qKWaj8FPzlhEuGUmam6bn2tIgBN', // Alchemy
+  'https://rpc.ankr.com/eth/8d7581cb1a742b4ebd60ddb0ff4049a193726fef2999a3acb4dc53293cf089b1' // Ankr
+];
 export const CONTRACT_ADDRESS = '0x6c1dA678A1B615f673208e74AB3510c22117090e';
-export const CONTRACT_ABI = PulseStrategyABI;
+export const CONTRACT_ABI = []; // In src/abi/PulseStrategy.json
 export const STAKED_PLS_ADDRESS = '0x0181e249c507d3b454dE2444444f0Bf5dBE72d09';
-export const INFURA_URL = 'https://mainnet.infura.io/v3/0c7b379c34424040826f02574f89b57d';
