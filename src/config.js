43c2a466bc33b333d3155358c0951a3dbe66aff7
KeepsItCrypt0 <PLSstrategy@gmail.com>
2025-05-06 21:52:29 -0500

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,6 @@
+import PulseStrategyABI from './abi/PulseStrategy.json';
+
+export const CONTRACT_ADDRESS = '0x6c1dA678A1B615f673208e74AB3510c22117090e';
+export const CONTRACT_ABI = PulseStrategyABI;
+export const STAKED_PLS_ADDRESS = '0x0181e249c507d3b454dE2444444f0Bf5dBE72d09';
+export const INFURA_URL = 'https://mainnet.infura.io/v3/0c7b379c34424040826f02574f89b57d';
