1dbb7ef4ffd7125321a11c9969edbdd34f41dcaf
KeepsItCrypt0 <PLSstrategy@gmail.com>
2025-05-06 22:41:37 -0500

    Update Disclaimer.js

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -1,9 +1,11 @@
-import { Text } from '@chakra-ui/react';
+import { Text, Box } from '@chakra-ui/react';
 
 const Disclaimer = () => (
-  <Text mt={6} fontSize="sm" color="gray.400" textAlign="center">
-    This is a decentralized finance application. Use at your own risk. Always do your own research. Not financial advice.
-  </Text>
+  <Box mt={8} textAlign="center">
+    <Text fontSize="sm" color="gray.400">
+      This is a decentralized finance application. Use at your own risk. Always do your own research. Not financial advice.
+    </Text>
+  </Box>
 );
 
 export default Disclaimer;
